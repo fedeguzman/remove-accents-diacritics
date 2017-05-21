7bc9ec802b5a52979dfeae221729de902c863cb3
Fede G <fedeguzman07@gmail.com>
2017-05-21 19:09:38 -0300

    init

---
+++ test.js
@@ -0,0 +1,79 @@
+/*
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+
+import test from 'ava';
+import remover from './index';
+
+test('guzmán', t => {
+    const n = remover.removeAccents('guzmán');
+    t.is(n, "guzman");
+})
+
+test('à la carte', t => {
+    const n = remover.removeAccents('à la carte');
+    t.is(n, "a la carte");
+})
+
+test('cafetière', t => {
+    const n = remover.removeAccents('cafetière');
+    t.is(n, "cafetiere");
+})
+
+test('garçonnière', t => {
+    const n = remover.removeAccents('garçonnière');
+    t.is(n, "garconniere");
+})
+
+test('galapeño', t => {
+    const n = remover.removeAccents('galapeno');
+    t.is(n, "galapeno");
+})
+
+test('langue d\'oïl', t => {
+    const n = remover.removeAccents('langue d\'oïl');
+    t.is(n, "langue d\'oil");
+})
+
+test('smörgåsbord', t => {
+    const n = remover.removeAccents('smörgåsbord');
+    t.is(n, "smorgasbord");
+})
+
+test('smörgåstårta', t => {
+    const n = remover.removeAccents('smörgåstårta');
+    t.is(n, "smorgastarta");
+})
+test('spermatozoön', t => {
+    const n = remover.removeAccents('spermatozoön');
+    t.is(n, "spermatozoon");
+})
+test('tête-à-tête', t => {
+    const n = remover.removeAccents('tête-à-tête');
+    t.is(n, "tete-a-tete");
+})
+test('zoölogy', t => {
+    const n = remover.removeAccents('zoölogy');
+    t.is(n, "zoology");
+})
+test('áéíóú', t => {
+    const n = remover.removeAccents('áéíóú');
+    t.is(n, "aeiou");
+})
+
+test('Rindfleischetikettierungsüberwachungsaufgabenübertragungsgesetz', t => {
+    const n = remover.removeAccents('Rindfleischetikettierungsüberwachungsaufgabenübertragungsgesetz');
+    t.is(n, "Rindfleischetikettierungsuberwachungsaufgabenubertragungsgesetz");
+})
